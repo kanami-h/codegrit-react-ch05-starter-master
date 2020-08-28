d336f2d71e62691c41efc961d1ef1469b34c7677
Kanami <kanami.h217@gmail.com>
2020-08-28 13:02:31 -0400

    added css

--- src/Styled/flexboxUtils.js
+++ src/Styled/flexboxUtils.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import styled from '@emotion/styled/macro';
 import PropTypes from 'prop-types';
 // ラベル追加の記述あとで必要かも？
 
-const FlexListContainer = styled.ul({}) => {
-  {
-    'display': 'flex',
-    'flexDirection': 'column',
-  },
-  props => {
-    let flexStyles = []
-    if(props.direction) {
-      styles.push({ 'flexDirection': 'row' })
-    } else {
-      styles.push({ 'flexDirection': 'column' })
-    }
-    return flexStyles;
-  }
-}
+const FlexListContainer = styled.ul({
+  display: 'flex'
+},({ direction }) => {})
+let flexStyles = []
+if(direction === 'column') {
+  styles.push({ flexDirection: 'row' })
+} 
+return flexStyles;
+
 
 export default FlexListContainer;
