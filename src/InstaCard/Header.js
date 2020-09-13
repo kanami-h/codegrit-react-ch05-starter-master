d2252e47ec8f37aeab863c946686936de707bf24
Kanami <kanami.h217@gmail.com>
2020-09-12 20:53:15 -0400

    added styled css

--- src/InstaCard/Header.js
+++ src/InstaCard/Header.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import Avatar from '../images/avatar-one.png';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 
-const Header = ({ data }) => (
-  <section className="card-header">
-    <div className="poster-avatar">
-      <img alt="アバター" src={data.posterAvatar} />
-    </div>
-    <div className="poster-name">
-      {data.poster}
-    </div>
-  </section>
-);
+const headerStyle = styled.section({
+  display: 'flex',
+  alignItems: 'center',
+  borderBottom: '1px solid #efefef',
+  padding: '16px',
+  height: '60px',
+})
+
+const posterAvatar = styled.div({
+  marginRight: '12px'
+})
+
+const posterAvatarImg = styled.img({
+  borderRadius: '50%',
+  border: '1px solid #dddddd',
+  height: '30px',
+  width: '30px',
+})
+
+
+const Header = ({ data }) => {
+  return (
+    <headerStyle>
+      <posterAvatar>
+        <posterAvatarImg alt="アバター" src={data.posterAvatar} />
+      </posterAvatar>
+      <div className="poster-name">
+        {data.poster}
+      </div>
+    </headerStyle>
+  )
+}
 
 Header.propTypes = {
   data: PropTypes.object
